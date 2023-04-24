e1fc0c7ea14c3a8c9cf03d75baa02f2e507fd049
Ariel Lin <al2248@cornell.edu>
2023-04-24 19:08:08 -0400

    Added webpack and package.json

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ["@babel/preset-env"], ["@babel/preset-react"]
+          ]
+        }
+      },
+    },
+    {
+      test: /.(css|scss)$/,
+      exclude: /node_modules/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+    }]
+  },
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'build'),
+      publicPath: '/build/bundle.js',
+    },
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:3000/',
+      }
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+  ],
+};
